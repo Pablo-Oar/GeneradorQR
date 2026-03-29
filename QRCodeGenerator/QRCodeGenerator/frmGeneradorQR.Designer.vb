b120414cf9a9a15d57863e13506f1810e64e44b8
Pablo <araujopablo95@gmail.com>
2026-03-29 05:48:23 -0300

    Generador QR

---
+++ QRCodeGenerator/QRCodeGenerator/frmGeneradorQR.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmGeneradorQR
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmGeneradorQR))
+        Me.btnGenerar = New System.Windows.Forms.Button()
+        Me.btnExportar = New System.Windows.Forms.Button()
+        Me.txtUrl = New System.Windows.Forms.TextBox()
+        Me.pbQr = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        CType(Me.pbQr, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnGenerar
+        '
+        Me.btnGenerar.FlatAppearance.BorderColor = System.Drawing.Color.Gray
+        Me.btnGenerar.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnGenerar.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnGenerar.Location = New System.Drawing.Point(374, 47)
+        Me.btnGenerar.Name = "btnGenerar"
+        Me.btnGenerar.Size = New System.Drawing.Size(135, 36)
+        Me.btnGenerar.TabIndex = 0
+        Me.btnGenerar.Text = "Generar"
+        Me.btnGenerar.UseVisualStyleBackColor = True
+        '
+        'btnExportar
+        '
+        Me.btnExportar.FlatAppearance.BorderColor = System.Drawing.Color.Gray
+        Me.btnExportar.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnExportar.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExportar.Location = New System.Drawing.Point(374, 99)
+        Me.btnExportar.Name = "btnExportar"
+        Me.btnExportar.Size = New System.Drawing.Size(135, 32)
+        Me.btnExportar.TabIndex = 1
+        Me.btnExportar.Text = "Exportar"
+        Me.btnExportar.UseVisualStyleBackColor = True
+        '
+        'txtUrl
+        '
+        Me.txtUrl.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.txtUrl.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtUrl.Location = New System.Drawing.Point(57, 10)
+        Me.txtUrl.Margin = New System.Windows.Forms.Padding(1)
+        Me.txtUrl.Name = "txtUrl"
+        Me.txtUrl.Size = New System.Drawing.Size(452, 20)
+        Me.txtUrl.TabIndex = 2
+        '
+        'pbQr
+        '
+        Me.pbQr.BackColor = System.Drawing.Color.Transparent
+        Me.pbQr.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.pbQr.Location = New System.Drawing.Point(124, 47)
+        Me.pbQr.Name = "pbQr"
+        Me.pbQr.Padding = New System.Windows.Forms.Padding(1, 0, 1, 0)
+        Me.pbQr.Size = New System.Drawing.Size(242, 246)
+        Me.pbQr.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.pbQr.TabIndex = 3
+        Me.pbQr.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label1.Font = New System.Drawing.Font("Cooper Black", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label1.Location = New System.Drawing.Point(9, 11)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 17)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "URL:"
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.FlatAppearance.BorderColor = System.Drawing.Color.Gray
+        Me.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnLimpiar.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLimpiar.Location = New System.Drawing.Point(374, 147)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(135, 32)
+        Me.btnLimpiar.TabIndex = 5
+        Me.btnLimpiar.Text = "Limpiar"
+        Me.btnLimpiar.UseVisualStyleBackColor = True
+        '
+        'frmGeneradorQR
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(519, 305)
+        Me.Controls.Add(Me.btnLimpiar)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.pbQr)
+        Me.Controls.Add(Me.txtUrl)
+        Me.Controls.Add(Me.btnExportar)
+        Me.Controls.Add(Me.btnGenerar)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "frmGeneradorQR"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Generador QR"
+        CType(Me.pbQr, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnGenerar As Button
+    Friend WithEvents btnExportar As Button
+    Friend WithEvents txtUrl As TextBox
+    Friend WithEvents pbQr As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnLimpiar As Button
+End Class
---
+++ QRCodeGenerator/QRCodeGenerator/frmGeneradorQR.vb
@@ -0,0 +1,81 @@
+﻿Imports ThoughtWorks.QRCode.Codec
+
+Public Class frmGeneradorQR
+
+    Dim qrEcoder As New QRCodeEncoder()
+    Private Sub btnGenerar_Click(sender As Object, e As EventArgs) Handles btnGenerar.Click
+
+
+        qrEcoder.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.BYTE
+        qrEcoder.QRCodeScale = 5
+
+        pbQr.Image = qrEcoder.Encode(txtUrl.Text)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+        Me.txtUrl.Text = String.Empty
+        pbQr.Image = Nothing
+    End Sub
+
+    Private Sub btnExportar_Click(sender As Object, e As EventArgs) Handles btnExportar.Click
+
+        If pbQr.Image Is Nothing Then
+            MessageBox.Show("Primero debe generar un código QR.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        Using saveFileDialog As New SaveFileDialog
+            saveFileDialog.Filter = "PNG Image|*.png|JPEG Image|*.jpg|Bitmap Image|*.bmp"
+            saveFileDialog.Title = "Guardar Imagen"
+            saveFileDialog.FileName = "CodigoQR"
+
+            If saveFileDialog.ShowDialog() = DialogResult.OK Then
+                'Guardo la imagen en la ruta seleccionada
+                Dim formato As Imaging.ImageFormat = Imaging.ImageFormat.Png
+                Select Case IO.Path.GetExtension(saveFileDialog.FileName).ToLower()
+                    Case ".jpg"
+                        formato = Imaging.ImageFormat.Jpeg
+                    Case ".bmp"
+                        formato = Imaging.ImageFormat.Bmp
+                End Select
+                pbQr.Image.Save(saveFileDialog.FileName, formato)
+                MessageBox.Show("Imagen guardada exitosamente.", "QRCode.Generator", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+
+        End Using
+    End Sub
+
+    Private Sub btnLimpiar_MouseHover(sender As Object, e As EventArgs) Handles btnLimpiar.MouseHover
+        btnLimpiar.BackColor = SystemColors.InactiveCaption
+        btnLimpiar.Font = New Font(btnLimpiar.Font.FontFamily, 12, FontStyle.Bold)
+    End Sub
+
+    Private Sub btnLimpiar_MouseLeave(sender As Object, e As EventArgs) Handles btnLimpiar.MouseLeave
+        btnLimpiar.BackColor = SystemColors.Control
+        btnLimpiar.Font = New Font(btnLimpiar.Font.FontFamily, 10, FontStyle.Bold)
+    End Sub
+
+    Private Sub btnExportar_MouseHover(sender As Object, e As EventArgs) Handles btnExportar.MouseHover
+        btnExportar.BackColor = SystemColors.InactiveCaption
+        btnExportar.Font = New Font(btnExportar.Font.FontFamily, 12, FontStyle.Bold)
+    End Sub
+    Private Sub btnExportar_MouseLeave(sender As Object, e As EventArgs) Handles btnExportar.MouseLeave
+        btnExportar.BackColor = SystemColors.Control
+        btnExportar.Font = New Font(btnExportar.Font.FontFamily, 10, FontStyle.Bold)
+    End Sub
+    Private Sub btnGenerar_MouseHover(sender As Object, e As EventArgs) Handles btnGenerar.MouseHover
+        btnGenerar.BackColor = SystemColors.InactiveCaption
+        btnGenerar.Font = New Font(btnGenerar.Font.FontFamily, 12, FontStyle.Bold)
+    End Sub
+    Private Sub btnGenerar_MouseLeave(sender As Object, e As EventArgs) Handles btnGenerar.MouseLeave
+        btnGenerar.BackColor = SystemColors.Control
+        btnGenerar.Font = New Font(btnGenerar.Font.FontFamily, 10, FontStyle.Bold)
+    End Sub
+
+    Private Sub txtUrl_MouseHover(sender As Object, e As EventArgs) Handles txtUrl.MouseHover
+        txtUrl.BackColor = SystemColors.GradientActiveCaption
+    End Sub
+    Private Sub txtUrl_MouseLeave(sender As Object, e As EventArgs) Handles txtUrl.MouseLeave
+        txtUrl.BackColor = SystemColors.Control
+    End Sub
+End Class
